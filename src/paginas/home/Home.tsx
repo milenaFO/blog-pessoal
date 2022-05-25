3dd8d4849315a748b096137bbd6d45ca745f6109
milenaFO <mmilenaf@gmail.com>
2022-05-25 00:49:03 -0300

    Início front BP

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Home.css"
+
+function Home (){
+    return (
+        // o <> é uma função abreviada
+        <> 
+            <h1 className="titulo">Home</h1>
+            <img src="http://pm1.narvii.com/6385/432a3dde8ecb3b81b0cfe0104fdb62370e394c9b_00.jpg" alt="Princesa Jujuba - Imagem tela inicial"className="img"></img>
+        </>
+    )
+}
+
+export default Home
