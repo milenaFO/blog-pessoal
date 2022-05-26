1d292cfd1ce0e7c622700b8d75aee93a983079a5
milenaFO <mmilenaf@gmail.com>
2022-05-25 23:40:07 -0300

    Home, Navbar e Footer do BP

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,14 +1,33 @@
-import React from "react"
-import "./Home.css"
+import React from "react";
+import {Typography, Box, Grid, Button} from '@material-ui/core';
+import "./Home.css";
 
-function Home (){
+
+
+function Home() {
     return (
         // o <> é uma função abreviada
-        <> 
-            <h1 className="titulo">Home</h1>
-            <img src="http://pm1.narvii.com/6385/432a3dde8ecb3b81b0cfe0104fdb62370e394c9b_00.jpg" alt="Princesa Jujuba - Imagem tela inicial"className="img"></img>
-        </>
-    )
-}
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
 
+        </>
+    );
+}
 export default Home
