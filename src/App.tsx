f9a990e46a7d1588bb564675167141b9d31a6b74
milenaFO <mmilenaf@gmail.com>
2022-05-27 18:00:22 -0300

    Router, route, routes e estilização

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
 import Home from "./paginas/home/Home";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from './paginas/login/Login';
 import './App.css';
 
 
 function App() {
-  return (
-    <>
-     <Navbar />
-     <Home />
-     <Footer />
-    </>
-  
-  );
+  return(
+    <Router>
+    <Navbar />
+    <div style={{ minHeight: '100vh' }}>
+    <Routes> // Antigo Switch
+    <Route path="/" element={<Login />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/home" element={<Home />} />
+    {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+    </Routes>
+    </div>
+    <Footer />
+    </Router>
+    )
 }
 
 export default App;
